ac35bceb131b69c27a0b4c14a4b5c4e54e2af38e
Aliens-Dev <merazganabil34@gmail.com>
2020-12-23 11:57:38 +0100

    multithreaded server, fix some bugs

--- client.py
+++ client.py
@@ -1,10 +1,12 @@
 import socket
+import sys
 
 
-HOST = '127.0.0.1'
-PORT = 60000
+HOST = input("Enter Server IP:")
+PORT = 5000
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    print(sys)
     s.connect((HOST,PORT))
     while True:
         msg = input("Please enter Message: ")
@@ -16,4 +18,3 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.sendall(bytes(msg, 'utf-8'))
             data = s.recv(1024)
 print('Disconnected')
-    
--- server.py
+++ server.py
@@ -1,31 +1,36 @@
 import socket 
 from concurrent import futures
+import sys
 
 HOST = '127.0.0.1' 
-PORT = 60000
+PORT = 5000
 list_threads = []
 
 def start_conn(conn,addr):
-        if(conn):
-            print(f'{addr} Connected')
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    print(f"{addr} disconnected")
-                    break
-                else:
-                    print(f"{data}")
-                    conn.sendall(data)
-            s.close()
+        print(f'{addr} Connected')
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                print(f"{addr} disconnected")
+                break
+            else:
+                print(f"{data}")
+                conn.sendall(data)
+
 
 with futures.ThreadPoolExecutor() as executer:
     while True:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((HOST,PORT))
-            s.listen()
-            conn, addr = s.accept()
-            thread = executer.submit(start_conn, conn,addr)
-            list_threads.append(thread)
+            try:
+                s.bind((HOST,PORT))
+                s.listen()
+                conn, addr = s.accept()
+                if(conn):
+                    thread = executer.submit(start_conn, conn,addr)
+                    list_threads.append(thread)
+            except socket.error:
+                print('Socket failed to create')
+                sys.exit()
 
 for thread in list_threads:
     thread.join()
